a59a5527579d991742f4e63762ac208d33dd852a
andrewkoshcheyev <andrewfanera@gmail.com>
2017-03-26 18:14:27 +0300

    Update AreaCalculator.java

--- quadrangle/src/main/java/com/koshcheyev/quadrangle/processing/AreaCalculator.java
+++ quadrangle/src/main/java/com/koshcheyev/quadrangle/processing/AreaCalculator.java
@@ -12,12 +12,11 @@ public class AreaCalculator {
 
     public static Double calculateArea(Quadrangle quadrangle){
         ArrayList<Point> vertices = quadrangle.getVertices();
-        //vertices.add(vertices.get(0));// ПРОВЕРИТЬ СОЗДАННЫЙ СПИСОК!!!
         double sum = 0.0;
         for (int i=0;i<quadrangle.getVerticesCount()-1; i++){
             sum+=(vertices.get(i).getX()+vertices.get(i+1).getY()-vertices.get(i).getY()*vertices.get(i+1).getX());
         }
-        return Math.abs(sum/2.0); // ПРОВЕРИТЬ ЕЩЕ РАЗ ФОРМУЛЫ СДЕЛАТЬ ПРОВЕРКУ НА ПРЯМОУГОЛЬНИКЕ
+        return Math.abs(sum/2.0);
     }
 
 }
